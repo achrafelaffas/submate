c6475a84f3479ab8b3386b77535c5ea9247c25cb
achraf <achrafelaffas@gmail.com>
2024-09-07 11:20:40 +0100

    Layout

---
+++ submate-ui/src/pages/Dashboard/Layout.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+import { Outlet } from "react-router-dom";
+
+const Dashboard = () => {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <Header />
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Dashboard;
