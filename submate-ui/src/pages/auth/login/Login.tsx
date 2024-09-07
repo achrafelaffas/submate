9aceaa97b1617388806c63d1cfa279d0a44d12de
achraf <achrafelaffas@gmail.com>
2024-09-07 11:20:28 +0100

    Login page

---
+++ submate-ui/src/pages/auth/login/Login.tsx
@@ -0,0 +1,119 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import z from "zod";
+import { AuthenticationControllerApi } from "@/api";
+import Spinner from "@/components/ui/spinner";
+
+const AuthRequestSchema = z.object({
+  email: z.string().email({ message: "Invalid email" }),
+  password: z.string().min(8, "Password should have 8 characters minimum"),
+});
+
+const Login = () => {
+  const auth = new AuthenticationControllerApi();
+  const form = useForm<z.infer<typeof AuthRequestSchema>>({
+    resolver: zodResolver(AuthRequestSchema),
+    defaultValues: {
+      password: "",
+      email: "",
+    },
+  });
+
+  const onSubmit = async (authRequest: z.infer<typeof AuthRequestSchema>) => {
+    await auth.authenticate(authRequest).then(
+      (token) => console.log(token),
+      (error) => console.error(error)
+    );
+  };
+
+  return (
+    <div className="flex h-screen items-center">
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div>
+            <Form {...form}>
+              <form
+                className="grid gap-4 w-full"
+                onSubmit={form.handleSubmit(onSubmit)}
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input {...field} placeholder="m@example.com" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          type="password"
+                          placeholder="password"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  type="submit"
+                  className="w-full"
+                  disabled={form.formState.isSubmitting}
+                >
+                  {form.formState.isSubmitting ? (
+                    <Spinner />
+                  ) : (
+                    "Login to your account"
+                  )}
+                </Button>
+              </form>
+            </Form>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link to="/register" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
