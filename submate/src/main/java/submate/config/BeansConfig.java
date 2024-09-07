b733e89b232b0040f4805a201ae57982e2bfd9b1
achraf <achrafelaffas@gmail.com>
2024-09-07 11:18:24 +0100

    update BeansConfig

--- submate/src/main/java/submate/config/BeansConfig.java
+++ submate/src/main/java/submate/config/BeansConfig.java
@@ -54,7 +54,7 @@ public class BeansConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.ORIGIN,
